76f5ddb8e73249223d1650aad37771d5fa8093f7
Henry McKay <56372705+hhmckay@users.noreply.github.com>
2023-04-19 09:48:08 -0700

    Update app.R

--- app.R
+++ app.R
@@ -1,6 +1,6 @@
-# Caltrans Accessibility Metric Builder
+# Conveyal Post-Processing Tool
 # Henry.McKay@dot.ca.gov
-# Last update: 08/25/2022
+# Last update: 04/18/2023
 
 # Load packages (will need to be installed first if not already)
 library(shiny)
@@ -27,10 +27,10 @@ tab1 <- tabPanel(
       helpText("Upload Conveyal Outputs"),
       fileInput("baseline_tiff", "Baseline Conveyal .TIFF Output", multiple = FALSE),
       fileInput("project_tiff", "Project Conveyal .TIFF Output", multiple = FALSE),
-      fileInput("weight_tiff", "Weight .TIFF (optional)", multiple = FALSE),
+      fileInput("weight_tiff", "Weight .TIFF", multiple = FALSE),
       fileInput("shapefile_input", "Project Extent Shapefile", multiple = T,
                 accept = c('.shp','.dbf','.sbn','.sbx','.shx','.prj', '.cpg')),
-      numericInput("buffer", "Analysis Area Buffer (miles)", 3),
+      numericInput("buffer", "Analysis Area Buffer (kilometers)", 3),
       download1 <- downloadButton('download1', "Export CSV")
     ),
     mainPanel(
@@ -47,11 +47,11 @@ tab3 <- tabPanel(
       selectInput("map_options", "Select Raster to Map", choices = c("Baseline Accessibility", 
                                                                      "Project Accessibility",
                                                                      "Accessibility Change",
-                                                                     "Weighted Accessibility Change",
-                                                                     "Baseline Accessibility (natural)", 
-                                                                     "Project Accessibility (natural)",
-                                                                     "Accessibility Change (natural)",
-                                                                     "Weighted Accessibility Change (natural)"))
+                                                                     "Weighted Accessibility Change")),
+      helpText("Note: The weighted accessibility change map is meant only to show where the relative
+               changes in accessibility occur when accounting for a weighitng factor, using a color scale. 
+               Individual numbers associated with these changes should only be analyzed in the context
+               of aggregate metrics, such as the ones shown on the project analysis tab.")
     ),
     mainPanel(
       tags$style(type = "text/css", "#map1 {height: calc(100vh - 100px) !important; }"),
@@ -62,7 +62,7 @@ tab3 <- tabPanel(
 
 # Assemble UI
 ui <- navbarPage(
-  title = "Caltrans Accessibility Metrics Post-Processing Tool",
+  title = "Conveyal Post-Processing Tool",
   tab1,
   tab3
 )
@@ -88,8 +88,8 @@ server <- function(input, output, session) {
     
     # Read shapefile from upload
     project_extent <- readOGR(paste(tempdirname,
-                         project_input$name[grep(pattern = "*.shp$", project_input$name)],
-                         sep = "/"
+                                    project_input$name[grep(pattern = "*.shp$", project_input$name)],
+                                    sep = "/"
     ))
     project_extent <- st_as_sf(project_extent)
     project_extent <- st_transform(project_extent, crs = 4326)
@@ -102,6 +102,7 @@ server <- function(input, output, session) {
     validate(
       need(input$baseline_tiff != "", "Please upload baseline .tiff file"),
       need(input$project_tiff != "", "Please upload a project .tiff file"),
+      need(input$weight_tiff != "", "Please upload a weight .tiff file"),
       need(input$shapefile_input != "", "Please upload a project shapefile")
     )
     
@@ -109,159 +110,74 @@ server <- function(input, output, session) {
     baseline <- raster(input$baseline_tiff$datapath)
     build <- raster(input$project_tiff$datapath)
     
-    ### Natural Study Area ##########################################################################
-    # Compute change in accessibility
-    difference_natural <- build - baseline
-    
-    # Calculate adjusted baseline
-    baseline_adj <- baseline
-    baseline_adj[] = ifelse(difference_natural[]!=0, baseline[], NA)
-    baseline_num_natural <- sum(values(baseline_adj), na.rm = T)
-    
-    # Calculate adjusted build
-    build_adj <- build
-    build_adj[] <- ifelse(difference_natural[]!=0, build[], NA)
-    build_num_natural <- sum(values(build_adj), na.rm = T)
-    
-    # Compute % change in accessibility
-    diff_pct_natural <- ((build_num_natural - baseline_num_natural) / baseline_num_natural)
-    
-    # Compute average change in accessibility
-    avg_diff_natural <- mean((values(build_adj) - values(baseline_adj)), na.rm = T)
-    
-    #################################################################################################
-    
     # Read in project shapefile and apply buffer at specified distance
     project_extent <- st_as_sf(project_extent())
     project_extent <- st_transform(project_extent, crs = 3857)
-    buffer <- st_buffer(project_extent, (input$buffer * 1609.34)) # Convert input (in miles) to meters
+    buffer <- st_buffer(project_extent, (input$buffer * 1000)) # Convert input (in kilometers) to meters
     buffer_diss <- st_as_sf(st_union(buffer))
     
-    # Calculate unweighted accessibility metrics
-    if(is.null(input$weight_tiff)) {
+    ### Calculate metrics
+    # Read weight raster, crop to buffer
+    weight <- raster(input$weight_tiff$datapath)
+    weight_cropped <- mask(weight, buffer_diss)
       
-      # Clip baseline .tiff to project area
-      baseline_cropped <- mask(baseline, buffer_diss)
-      # Sum accessibility values for all raster cells within the buffer
-      baseline_num <- sum(values(baseline_cropped), na.rm = T)
+    # Baseline
+    baseline_cropped <- mask(baseline, buffer_diss)
+    baseline_num <- sum(values(baseline_cropped), na.rm = T)
       
-      # Build
-      build_cropped <- mask(build, buffer_diss)
-      # Clip build .tiff to project area
-      build_num <- sum(values(build_cropped), na.rm = T)
+    # Build
+    build_cropped <- mask(build, buffer_diss)
+    build_num <- sum(values(build_cropped), na.rm = T)
       
-      # Compute % change in accessibility
-      diff_pct <- ((build_num - baseline_num) / baseline_num)
+    # Difference
+    difference <- build_cropped - baseline_cropped
       
-      # Compute average change in accessibility
-      avg_diff <- mean((values(build_cropped) - values(baseline_cropped)), na.rm = T)
+    # Weight baseline and build .TIFFs
+    baseline_weighted <- baseline * weight_cropped
+    build_weighted <- build * weight_cropped
       
-      # Create summary data frame
-      project <- input$project_name
-      pct_change_access <- diff_pct
-      pct_change_access_natural <- diff_pct_natural
-      avg_change_access <- avg_diff
-      avg_change_access_natural <- avg_diff_natural
-      eval_table <- data.frame(project, pct_change_access, pct_change_access_natural, avg_change_access, avg_change_access_natural)
+    # Weighted Baseline
+    w_baseline_cropped <- mask(baseline_weighted, buffer_diss)
+    w_baseline_num <- sum(values(w_baseline_cropped), na.rm = T)
+      
+    # Weighted Build
+    w_build_cropped <- mask(build_weighted, buffer_diss)
+    w_build_num <- sum(values(w_build_cropped), na.rm = T)
     
-    # Calculate weighted accessibility metrics
-    } else {
+    # Compute weighted % change in accessibility
+    weighted_diff_pct <- ((w_build_num - w_baseline_num) / w_baseline_num)
+    ###
       
-      # Read weight raster, crop to buffer
-      weight <- raster(input$weight_tiff$datapath)
-      weight_cropped <- mask(weight, buffer_diss)
+    # Compute average change in accessibility
+    weighted_access_change <- weighted.mean(values(difference), values(weight_cropped), na.rm = T)
       
-      # Baseline
-      baseline_cropped <- mask(baseline, buffer_diss)
-      baseline_num <- sum(values(baseline_cropped), na.rm = T)
+    # Compute % change in accessibility
+    diff_pct <- ((build_num - baseline_num) / baseline_num)
       
-      # Build
-      build_cropped <- mask(build, buffer_diss)
-      build_num <- sum(values(build_cropped), na.rm = T)
+    # Compute average change in accessibility
+    avg_diff <- mean((values(build_cropped) - values(baseline_cropped)), na.rm = T)
       
-      # Difference
-      difference <- build_cropped - baseline_cropped
+    # Create summary data frame
+    project <- input$project_name
+    pct_change_access <- diff_pct
+    pct_change_access_weighted <- weighted_diff_pct
+    avg_change_access <- avg_diff
+    weighted_avg_change_access <- weighted_access_change
       
-      # Weight baseline and build .TIFFs
-      baseline_weighted <- baseline * weight_cropped
-      build_weighted <- build * weight_cropped
-    
-      # Weighted Baseline
-      w_baseline_cropped <- mask(baseline_weighted, buffer_diss)
-      w_baseline_num <- sum(values(w_baseline_cropped), na.rm = T)
-      
-      # Weighted Build
-      w_build_cropped <- mask(build_weighted, buffer_diss)
-      w_build_num <- sum(values(w_build_cropped), na.rm = T)
-      
-      ### Natural #######################################################################################
-      # Calculate adjusted difference
-      difference_adjusted <- difference_natural
-      difference_adjusted[] = ifelse(difference[]!=0, difference[], NA)
-      
-      # Calculate adjusted weight
-      weight_adjusted <- weight
-      weight_adjusted[] = ifelse(difference_natural[] != 0, weight[], NA)
-      
-      # Calculate weighted raster
-      weighted_access_natural <- weighted.mean(values(difference_adjusted), values(weight_adjusted), na.rm = T)
-      
-      
-      # Compute weighted % change in accessibility
-      baseline_num_weighted <- sum(values(baseline_adj * weight_adjusted), na.rm = T)
-      build_num_weighted <- sum(values(build_adj * weight_adjusted), na.rm = T)
-      weighted_diff_pct_natural <- ((build_num_weighted - baseline_num_weighted) / baseline_num_weighted)
-      
-      # Compute average change in accessibility
-      avg_diff_natural <- mean((values(build_adj) - values(baseline_adj)), na.rm = T)
-      
-      # Compute weighted % change in accessibility
-      weighted_diff_pct <- ((w_build_num - w_baseline_num) / w_baseline_num)
-      ###
-      
-      # Compute average change in accessibility
-      weighted_access_change <- weighted.mean(values(difference), values(weight_cropped), na.rm = T)
-      
-      # Compute % change in accessibility
-      diff_pct <- ((build_num - baseline_num) / baseline_num)
-      
-      # Compute average change in accessibility
-      avg_diff <- mean((values(build_cropped) - values(baseline_cropped)), na.rm = T)
-      
-      # Create summary data frame
-      project <- input$project_name
-      pct_change_access <- diff_pct
-      pct_change_access_weighted <- weighted_diff_pct
-      pct_change_access_natural <- diff_pct_natural
-      pct_change_access_weighted_natural <- weighted_diff_pct_natural
-      avg_change_access <- avg_diff
-      weighted_avg_change_access <- weighted_access_change
-      avg_change_access_natural <- avg_diff_natural
-      weighted_avg_change_access_natural <- weighted_access_natural
-      
-      eval_table <- data.frame(project, pct_change_access, pct_change_access_weighted, pct_change_access_natural, pct_change_access_weighted_natural, avg_change_access, weighted_avg_change_access, avg_change_access_natural, weighted_avg_change_access_natural)
-    }
+    eval_table <- data.frame(project, 
+                             pct_change_access, 
+                             pct_change_access_weighted, 
+                             avg_change_access, 
+                             weighted_avg_change_access)
   })
   
   # Render/format data table
   output$table1 = renderDataTable({
-    if (is.null(input$weight_tiff)) {
-    datatable(eval_table()) %>%
-      formatPercentage(c("pct_change_access"), 2) %>%
-      formatPercentage(c("pct_change_access_natural"), 2) %>% 
-      formatCurrency(c("avg_change_access"), currency = "", interval = 3, mark = ",", digits = 0) %>%
-      formatCurrency(c("avg_change_access_natural"), currency = "", interval = 3, mark = ",", digits = 0)
-    } else {
       datatable(eval_table()) %>%
         formatPercentage(c("pct_change_access"), 2) %>%
         formatPercentage(c("pct_change_access_weighted"), 2) %>%
-        formatPercentage(c("pct_change_access_natural"), 2) %>% 
-        formatPercentage(c("pct_change_access_weighted_natural"), 2) %>% 
-        formatCurrency(c("avg_change_access"), currency = "", interval = 3, mark = ",", digits = 0) %>%
-        formatCurrency(c("weighted_avg_change_access"), currency = "", interval = 3, mark = ",", digits = 0) %>%
-        formatCurrency(c("avg_change_access_natural"), currency = "", interval = 3, mark = ",", digits = 0) %>%
-        formatCurrency(c("weighted_avg_change_access_natural"), currency = "", interval = 3, mark = ",", digits = 0)
-    }
+        formatCurrency(c("avg_change_access"), currency = "", interval = 3, mark = ",", digits = 2) %>%
+        formatCurrency(c("weighted_avg_change_access"), currency = "", interval = 3, mark = ",", digits = 2)
   })
   
   # Download summary table as csv
@@ -283,6 +199,7 @@ server <- function(input, output, session) {
     validate(
       need(input$baseline_tiff != "", "Please upload a baseline .tiff file"),
       need(input$project_tiff != "", "Please upload a project .tiff file"),
+      need(input$weight_tiff != "", "Please upload a weight .tiff file"),
       need(input$shapefile_input != "", "Please upload a project shapefile")
     )
     
@@ -292,7 +209,7 @@ server <- function(input, output, session) {
     project_extent <- st_as_sf(project_extent())
     project_extent <- st_transform(project_extent, crs = 3857)
     
-    buffer <- st_buffer(project_extent, (input$buffer * 1609.34))
+    buffer <- st_buffer(project_extent, (input$buffer * 1000))
     buffer_diss <- st_as_sf(st_union(buffer))
     
     # Read in baseline and build .TIFF files
@@ -302,22 +219,9 @@ server <- function(input, output, session) {
     # Compute change in accessibility
     difference_clipped <- mask((build - baseline), buffer_diss)
     
-    # Temporarily disabled the weighting from the map option since I have to rewrite the front end for it
+    # Weighting
     clipped_weight <- mask(raster(input$weight_tiff$datapath), buffer_diss)
     weighted_differences_clipped <- (build_clipped * clipped_weight) - (baseline_clipped * clipped_weight)
-    
-    ### Natural ################################################################################################
-    # Compute change in accessibility
-    difference <- build - baseline
-    difference[] = ifelse(difference[]!=0, difference[], NA)
-    
-    # Compute weighted change in accessibility
-    weight_adjusted <- raster(input$weight_tiff$datapath)
-    weight_adjusted[] = ifelse(difference[] != 0, weight_adjusted[], NA)
-    
-    weighted_differences <- weight_adjusted * difference
-    
-   # weighted_differences_clipped <- weight_adjusted * difference
     
     if(input$map_options == "Baseline Accessibility") {
       raster_file <- baseline_clipped
@@ -327,14 +231,6 @@ server <- function(input, output, session) {
       raster_file <- difference_clipped
     } else if(input$map_options == "Weighted Accessibility Change") {
       raster_file <- weighted_differences_clipped
-    } else if(input$map_options == "Baseline Accessibility (natural)") {
-      raster_file <- baseline
-    } else if(input$map_options == "Project Accessibility (natural)") {
-      raster_file <- build
-    } else if(input$map_options == "Accessibility Change (natural)") {
-      raster_file <- difference
-    } else if(input$map_options == "Weighted Accessibility Change (natural)") {
-      raster_file <- weighted_differences
     }
     
     # Define color pallette
@@ -342,7 +238,7 @@ server <- function(input, output, session) {
     
     # Render leaflet map
     leaflet() %>%
-    addProviderTiles(providers$CartoDB.Positron) %>%
+      addProviderTiles(providers$CartoDB.Positron) %>%
       addRasterImage(raster_file, colors = pal, opacity = .6, project = F) %>%
       addPolylines(data = project_extent()) %>%
       addLegend(pal = pal, values = values(raster_file), title = input$map_options) %>%
